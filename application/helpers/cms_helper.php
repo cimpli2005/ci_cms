fb1f9a5cf315c9da7f41d191d5c67a7258d186f5
cimpli2005 <70048710+cimpli2005@users.noreply.github.com>
2020-09-29 15:11:57 +0700

    add frontend

--- application/helpers/cms_helper.php
+++ application/helpers/cms_helper.php
@@ -11,37 +11,71 @@ function btn_delete($uri)
 	));
 }
 
+function e($string)
+{
+	return htmlentities($string);
+}
+
+function get_menu($array, $child = FALSE)
+{
+	$CI =& get_instance();
+	$str = '';
+
+	if (count($array)) {
+		//$str .= $child == FALSE ? '<li class="nav-item">' . PHP_EOL : '<li class="nav-item dropdown">' . PHP_EOL;
+
+		foreach ($array as $item) {
+			if (isset($item['children']) && count($item['children'])) {
+				$str .= '<li class="nav-item dropdown">';				
+				$str .= '<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="' . base_url(e($item['slug'])) . '">' . e($item['title']) . '</a>' . PHP_EOL;
+				$str .= '<div class="dropdown-menu"' . PHP_EOL;
+				$str .= get_menu($item['children'], TRUE);
+				$str .= '</div>' . PHP_EOL;
+			} else {
+				$str .= '<li class="nav-item">';
+				$str .= '<a href="' . base_url(e($item['slug'])) . '" class="nav-link">' . e($item['title']) . '</a>';
+			}			
+
+			$str .= '</li>' . PHP_EOL;
+		}
+
+		//$str .= '</li>' . PHP_EOL;
+	}
+
+	return $str;
+}
+
 /**
  * Dump helper. Functions to dump variables to the screen, in a nicley formatted manner.
  * @author Joost van Veen
  * @version 1.0
  */
 if (!function_exists('dump')) {
-    function dump ($var, $label = 'Dump', $echo = TRUE)
-    {
-        // Store dump in variable 
-        ob_start();
-        var_dump($var);
-        $output = ob_get_clean();
-        
-        // Add formatting
-        $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
-        $output = '<pre style="background: #FFFEEF; color: #000; border: 1px dotted #000; padding: 10px; margin: 10px 0; text-align: left;">' . $label . ' => ' . $output . '</pre>';
-        
-        // Output
-        if ($echo == TRUE) {
-            echo $output;
-        }
-        else {
-            return $output;
-        }
-    }
+	function dump ($var, $label = 'Dump', $echo = TRUE)
+	{
+		// Store dump in variable 
+		ob_start();
+		var_dump($var);
+		$output = ob_get_clean();
+		
+		// Add formatting
+		$output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+		$output = '<pre style="background: #FFFEEF; color: #000; border: 1px dotted #000; padding: 10px; margin: 10px 0; text-align: left;">' . $label . ' => ' . $output . '</pre>';
+		
+		// Output
+		if ($echo == TRUE) {
+			echo $output;
+		}
+		else {
+			return $output;
+		}
+	}
 }
 
 
 if (!function_exists('dump_exit')) {
-    function dump_exit($var, $label = 'Dump', $echo = TRUE) {
-        dump ($var, $label, $echo);
-        exit;
-    }
+	function dump_exit($var, $label = 'Dump', $echo = TRUE) {
+		dump ($var, $label, $echo);
+		exit;
+	}
 }
--- application/views/_main_layout.php
+++ application/views/_main_layout.php
@@ -6,19 +6,20 @@
 	
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
-			<ul class="navbar-nav">
+			<ul class="nav nav-pills">
+				<!--
 				<li class="nav-item dropdown">
-					<a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Link
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<a href="#" class="dropdown-item">Link</a>
-						<a href="#" class="dropdown-item">Link</a>
-						<a href="#" class="dropdown-item">Link</a>
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Dropdown</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="#">Link 1</a>
+						<a class="dropdown-item" href="#">Link 2</a>
+						<a class="dropdown-item" href="#">Link 3</a>
 					</div>
 				</li>
-				<li class="nav-item"><a href="#" class="nav-link">Link</a></li>
-				<li class="nav-item"><a href="#" class="nav-link">Link</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Link 1</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Link 2</a></li>
+			-->
+			<?php echo get_menu($menu); ?>
 			</ul>
 		</div>
 	</nav>
